ceacb73b4d24fda0a2123f30fe553f460a16b476
rayziwu <rayziwu@gmail.com>
2026-01-03 10:03:37 +0800

    Change version no to 3.4.1

--- StockFetcher/app/build.gradle.kts
+++ StockFetcher/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 2
-        versionName = "3.4.0"
+        versionName = "3.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
